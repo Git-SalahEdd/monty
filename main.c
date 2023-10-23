72d383014507d59386598198a4dd364b52d11009
Precious Aleaji <legenduz15@gmail.com>
2023-10-23 03:13:19 +0100

    betty

--- main.c
+++ main.c
@@ -45,7 +45,8 @@ void switch_opcodes(stack_t **head, char *word, unsigned int line_num)
 		swap(head, line_num);
 	else if (strcmp(word, "add") == 0)
 		add(head, line_num);
-	else if (strcmp(word, "nop") == 0) {}
+	else if (strcmp(word, "nop") == 0)
+		; /* do nothing */
 	else if (strcmp(word, "sub") == 0)
 		sub(head, line_num);
 	else if (strcmp(word, "div") == 0)
